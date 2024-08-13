4ef910794160f056697fd9119b7494c8d191d0dd
Alberto Iong <albert0i@hotmail.com>
2024-08-13 15:24:45 +0800

    write more

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -1,10 +1,17 @@
 import { Redis } from 'ioredis'
 
+/**
+ * Create a Redis instance. 
+ * @returns {redisClient} The Redis Client instance 
+ */
 const redisClient = new Redis({
     port: 7000, // Redis port
-    host: "127.0.0.1", // Redis in localhost 
+    host: "127.0.0.1", // a Redis instance.
   });
 
+/**
+ * Close Redis connection 
+ */
 const disconnect = () => {
   redisClient.disconnect()
 }
