d896dc52b7d4bfbf861c3f23da5b976c88b2df5c
Alberto Iong <albert0i@hotmail.com>
2024-08-15 11:25:50 +0800

    add mysql dao

--- src/daos/impl/mysql/posts_dao_mysql_impl.js
+++ src/daos/impl/mysql/posts_dao_mysql_impl.js
@@ -1,56 +1,10 @@
-import { redisClient } from './redisClient.js'
-import { findAllWithLua } from './scripts/findAll_script.js'
-import { getPostHashKey, getPostIDsKey } from './redis_key_generator.js'
+// import { redisClient } from './redisClient.js'
+// import { findAllWithLua } from './scripts/findAll_script.js'
+// import { getPostHashKey, getPostIDsKey } from './redis_key_generator.js'
 
-/**
- * Takes a key/value pairs object representing a Redis hash, and
- * returns a new object whose structure matches that of the post domain
- * object.  Also converts fields whose values are numbers back to
- * numbers as Redis stores all hash key values as strings.
- *
- * @param {Object} postHash - object containing hash values from Redis
- * @returns {Object} - object containing the values from Redis remapped
- *  to the shape of a post domain object.
- * @private
- */
-const remap = (postHash) => {
-  const remappedPostHash = { ...postHash };
+import { PrismaClient } from '@prisma/client'
 
-  remappedPostHash.id = parseInt(postHash.id, 10);
-  remappedPostHash.userId = parseInt(postHash.userId, 10);
-
-  return remappedPostHash;
-};
-
-/**
- * Takes a array of array object representing a Redis hash, and
- * returns a new object whose structure matches that of the post domain
- * object.  Also converts fields whose values are numbers back to
- * numbers as Redis stores all hash key values as strings.
- *
- * @param {array} arr - array of array containing hash values from Redis
- * @returns {array} - array of object containing the values from Redis remapped
- *  to the shape of a post domain object.
- * @private
- */
-function retrofit(arr) {
-  let ret = []
-
-  for (let i=0; i < arr.length; i++ ) {
-      const element = arr[i]
-      let obj = {}
-      for (let j=0; j < element.length; j+=2 ) {
-          const key = element[j]
-          const value = element[j+1]
-          if (key === 'id' || key === 'userId') 
-              obj[key] = parseInt(value, 10)
-          else
-              obj[key] = value
-      }
-      ret.push(obj)
-  }
-  return ret
-}
+const prisma = new PrismaClient()
 
 /**
  * Insert a new post.
@@ -60,50 +14,42 @@ function retrofit(arr) {
  *   for the key of the post Redis.
  */
 const insert = async (post) => {
-  const id = post.id
-  const postHashKey = getPostHashKey(id);
-  const postIDsKey = getPostIDsKey()
+  return prisma.posts.create({ data: post })
+}
 
-  return await redisClient.multi()
-                          .hmset(postHashKey, post)           // 'OK' 
-                          .zadd(postIDsKey, id, postHashKey)  // 1
-                          .exec()
-  // [ [ null, 'OK' ], [ null, 1 ] ]
-};
-
-/**
- * Update a post.
- *
- * @param {Object} post - a post object.
- * @returns {Promise} - a Promise, resolving to the string value
- *   for the key of the post Redis.
- */
-const update = async (post) => {
-  const id = post.id
-  const postHashKey = getPostHashKey(id);
+// /**
+//  * Update a post.
+//  *
+//  * @param {Object} post - a post object.
+//  * @returns {Promise} - a Promise, resolving to the string value
+//  *   for the key of the post Redis.
+//  */
+// const update = async (post) => {
+//   const id = post.id
+//   const postHashKey = getPostHashKey(id);
   
-  return await redisClient.hmset(postHashKey, post);  
-  // OK
-};
+//   return await redisClient.hmset(postHashKey, post);  
+//   // OK
+// };
 
-/**
- * Delete a post.
- *
- * @param {Object} post - a post object.
- * @returns {Promise} - a Promise, resolving to the string value
- *   for the key of the post Redis.
- */
-const del = async (id) => {
-  const postHashKey = getPostHashKey(id);
-  const postIDsKey = getPostIDsKey()
+// /**
+//  * Delete a post.
+//  *
+//  * @param {Object} post - a post object.
+//  * @returns {Promise} - a Promise, resolving to the string value
+//  *   for the key of the post Redis.
+//  */
+// const del = async (id) => {
+//   const postHashKey = getPostHashKey(id);
+//   const postIDsKey = getPostIDsKey()
 
   
-  return await redisClient.multi()
-                          .del(postHashKey)               // 1
-                          .zrem(postIDsKey, postHashKey)  // 1
-                          .exec()
-  // [ [ null, 1 ], [ null, 1 ] ]
-};
+//   return await redisClient.multi()
+//                           .del(postHashKey)               // 1
+//                           .zrem(postIDsKey, postHashKey)  // 1
+//                           .exec()
+//   // [ [ null, 1 ], [ null, 1 ] ]
+// };
 
 /**
  * Get the post object for a given post ID.
@@ -112,10 +58,7 @@ const del = async (id) => {
  * @returns {Promise} - a Promise, resolving to a post object.
  */
 const findById = async (id) => {
-  const postKey = getPostHashKey(id);
-  const postHash = await redisClient.hgetall(postKey);
-
-  return (Object.keys(postHash) == 0 ? null : remap(postHash));
+  return prisma.posts.findUnique({ where: { id } })
 };
 
 /**
@@ -124,28 +67,7 @@ const findById = async (id) => {
  * @returns {Promise} - a Promise, resolving to an array of post objects.
  */
 const findAll = async () => {    
-  const postIDsKey = getPostIDsKey() 
-  let allPosts = []; 
-
-  // Method 1 - JS code 
-  // const ids = await redisClient.zrange(postIDsKey, 0, -1)
-  // let words = null; 
-  // let id = null
-  // let postHash = null; 
-  
-  // for (let i=0; i < ids.length; i++) {
-  //   words = ids[i].split(':')
-  //   id = words[words.length-1]
-
-  //   postHash = await findById(id);
-  //   allPosts.push(postHash)
-  // }
-
-  // Method 2 - Lua script 
-  // Script is loaded in redisClient.js 
-  allPosts = retrofit(await findAllWithLua(postIDsKey)) 
-
-  return allPosts; 
+  return prisma.posts.findMany({ })
 };
 
 /**
@@ -154,10 +76,18 @@ const findAll = async () => {
  * @returns {void}
  */
 const disconnect = async () => {
-  redisClient.disconnect()
+  await prisma.$disconnect()
 }
 
 
+// export {
+//   insert, update, del, findById, findAll, disconnect
+// };
 export {
-  insert, update, del, findById, findAll, disconnect
+  insert, findById, findAll, disconnect
 };
+
+/*
+   Prisma | CRUD 
+   https://www.prisma.io/docs/orm/prisma-client/queries/crud
+*/
---
+++ test/dao-mysql.test.js
@@ -0,0 +1,84 @@
+//import { insert, update, del, findById, findAll, disconnect } from "../src/daos/posts_dao.js"
+import { insert, findById, findAll, disconnect } from "../src/daos/impl/mysql/posts_dao_mysql_impl.js"
+
+const testSuiteName = 'post_dao_redis_impl';
+
+afterAll( async () => { 
+  // Release Redis connection.
+  await disconnect()
+} );
+
+test(`${testSuiteName}: findById with existing post`, async () => {
+    const data = {
+        userId: 7,
+        id: 66,
+        title: "repudiandae ea animi iusto",
+        body: "officia veritatis tenetur vero qui itaque\nsint non ratione\nsed et ut asperiores iusto eos molestiae nostrum\nveritatis quibusdam et nemo iusto saepe"
+      }
+    const id = 66
+    const post = findById(id);
+    
+    expect(post).resolves.toEqual(data)
+  })
+
+test(`${testSuiteName}: findById with missing post`, async () => {
+  const id = 999
+  const post = findById(id);
+  
+  expect(post).resolves.toBe(null);
+});
+
+test(`${testSuiteName}: findAll posts`, async () => {
+  const data = {
+      userId: 9,
+      id: 88,
+      title: "sapiente omnis fugit eos",
+      body: "consequatur omnis est praesentium\nducimus non iste\nneque hic deserunt\nvoluptatibus veniam cum et rerum sed"
+    }
+  const posts = await findAll();
+  const singlePost = posts.filter(post => post.id === data.id);
+  
+  expect(posts.length).toEqual(100);
+  expect(singlePost[0]).toEqual(data)
+});
+
+
+test(`${testSuiteName}: insert a post`, async () => {
+  const data = {
+      userId: 999,
+      id: 999,
+      title: "《史記‧商君列傳》",
+      body: "公叔既死，公孫鞅聞秦孝公下令國中求賢者，將修繆公之業，東復侵地，乃遂西入秦，因孝公寵臣景監以求見孝公。"
+    }
+  
+  const result = await insert(data);
+  expect([ [ null, 'OK' ], [ null, 1 ] ]).toEqual(expect.arrayContaining(result));
+  
+  const post = findById(data.id)
+  expect(post).resolves.toEqual(data);
+});
+
+test.skip(`${testSuiteName}: update a post`, async () => {
+  const data = {
+      userId: 999,
+      id: 999,
+      title: "死人頭",
+      body: "鞅曰：「吾說君以帝王之道比三代，而君曰：『久遠，吾不能待。且賢君者，各及其身顯名天下，安能邑邑待數十百年以成帝王乎？』故吾以彊國之術說君，君大說之耳。然亦難以比德於殷周矣。」"
+    }
+  
+  const result = await update(data);
+  expect(result).toBe('OK')
+
+  const post = findById(data.id)  
+  expect(post).resolves.toEqual(data);
+});
+
+test.skip(`${testSuiteName}: delete a post`, async () => {
+  const id = 999
+  
+  const result = await del(id);
+  expect([ [ null, 1 ], [ null, 1 ] ]).toEqual(expect.arrayContaining(result));
+
+  const post = findById(id)  
+  expect(post).resolves.toBe(null);
+});
